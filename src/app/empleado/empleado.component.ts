d6eb40906d51bfc50e5dcd247014f8802b25d8c2
JUNIOR-SANCHEZ <junior92.cancer@gmail.com>
2018-04-07 22:20:33 -0500

    udemy Crear clases, modelos de datos y datos

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './empleado'
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+})
+export class EmpleadoComponent implements OnInit {
+  public title:string;
+  public employee:Employee;
+  public workers:Array<Employee>; 
+  constructor() {
+    this.title = "Curso de Angular";
+    this.employee = new Employee("Julio",26,"Programador",false);
+    this.workers=[
+      new Employee("Juan Aguirre",28,"Programador",true),
+      new Employee("Manuel Perez",28,"Disñador",true),
+      new Employee("Lorena Zambrano",24,"Farmaceutica",false),
+      new Employee("Monica Sanchez",25,"Secretaria",true)
+    ]
+   }
+
+  ngOnInit() {
+  }
+
+}
