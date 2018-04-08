918419c58cc950dc5f840272b77971af80c2acc3
JUNIOR-SANCHEZ <junior92.cancer@gmail.com>
2018-04-07 22:48:32 -0500

    udemy Directiva ngIf y evento click

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -9,18 +9,23 @@ export class EmpleadoComponent implements OnInit {
   public title:string;
   public employee:Employee;
   public workers:Array<Employee>; 
+  public externalWorker:boolean;
   constructor() {
     this.title = "Curso de Angular";
-    this.employee = new Employee("Julio",26,"Programador",false);
+    this.employee = new Employee("Julio Sánchez",26,"Programador",false);
     this.workers=[
       new Employee("Juan Aguirre",28,"Programador",true),
       new Employee("Manuel Perez",28,"Disñador",true),
       new Employee("Lorena Zambrano",24,"Farmaceutica",false),
       new Employee("Monica Sanchez",25,"Secretaria",true)
-    ]
+    ];
+    this.externalWorker = false;
    }
 
   ngOnInit() {
   }
+  changeExternal(value){
+    this.externalWorker = value;
+  };
 
 }
