119232226f986ffdb31f89a1d6e8465c72481635
JUNIOR-SANCHEZ <junior92.cancer@gmail.com>
2018-04-08 14:26:12 -0500

    udemy Pipes para fechas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,12 @@ export class HomeComponent implements OnInit {
   public title: string;
   public listRopa: Array<string>;
   public newRopa: string;
+  public fecha;
   constructor(
     private _ropaService: RopaService
   ) {
     this.title = "Componente Home";
+    this.fecha = new Date(2018,4,8);
    }
 
   ngOnInit() {
